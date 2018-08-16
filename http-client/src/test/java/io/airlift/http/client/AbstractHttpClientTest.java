f1d17b1fcf2af00fc56bd3f9138993f149168112
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2018-08-16 10:52:59 -0500

    Trying to fit within Travis's 4MB log limit

--- http-client/src/test/java/io/airlift/http/client/AbstractHttpClientTest.java
+++ http-client/src/test/java/io/airlift/http/client/AbstractHttpClientTest.java
@@ -393,7 +393,7 @@ public abstract class AbstractHttpClientTest
         assertEquals(servlet.getRequestUri(), uri);
     }
 
-    @Test(invocationCount = 500)
+    @Test
     public void testKeepAlive()
             throws Exception
     {
